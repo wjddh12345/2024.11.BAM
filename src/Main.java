7ce720f448c02fcb9298c459d0c4115ae0aaa102
wjddh12345 <wjddh1121@naver.com>
2025-01-04 16:54:19 +0900

    article write, article list

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -6,6 +8,10 @@ public class Main {
 		System.out.println("== 프로그램 시작 ==");
 		
 		Scanner sc = new Scanner(System.in);
+		
+		int lastArticleId = 0;
+		
+		List<Article> articles = new ArrayList<>();
 		
 		while(true) {
 			System.out.printf("명령어) ");
@@ -19,10 +25,58 @@ public class Main {
 			if (cmd.equals("exit")) {
 				break;
 			}
+			
+			if(cmd.equals("article write")) {
+				
+				lastArticleId++;
+				
+				System.out.printf("제목 : ");
+				String title = sc.nextLine();
+				System.out.printf("내용 : ");
+				String body = sc.nextLine();
+				
+				Article article = new Article(lastArticleId, title, body);
+				
+				articles.add(article);
+				
+				System.out.printf("%d번 게시글 생성\n", lastArticleId);
+			}
+			
+			else if(cmd.equals("article list")) {
+				
+				if(articles.size() == 0) {
+					System.out.println("게시물이 존재하지 않습니다.");
+					continue;
+				}
+				
+				System.out.println("번호       /       제목");
+				for (int i = articles.size() - 1; i >= 0; i--) {
+					Article article = articles.get(i);
+					System.out.printf("%d       /       %s\n", article.id, article.title);
+				}
+			}
+			
+			else {
+				System.out.println("존재하지 않는 명령어 입니다.");
+			}
+			
 		}
 		
 		System.out.println("== 프로그램 끝 ==");
 		
 		sc.close();
+	}
+	
+}
+
+class Article {
+	int id;
+	String title;
+	String body;
+	
+	Article(int id, String title, String body) {
+		this.id = id;
+		this.title = title;
+		this.body = body;
 	}
 }
